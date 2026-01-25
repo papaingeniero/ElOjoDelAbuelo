a5cf06a3918ce319111e87030db53f5fbaf9be91
papaingeniero <soypapaingeniero@gmail.com>
2026-01-25 21:10:52 +0100

    fix(web): add thumbnail retry mechanism on load error

--- app/src/main/java/com/elojodelabuelo/NanoHttpServer.java
+++ app/src/main/java/com/elojodelabuelo/NanoHttpServer.java
@@ -1481,8 +1481,7 @@ public class NanoHttpServer {
                 "        \n" +
                 "        // Injected canvas for animation\n" +
                 "        var innerContent = \"<div class='thumb-container'>\" +\n" +
-                "            \"<img class='thumb' src='/thumbnails/\" + filename + \"?t=\" + Date.now() + \"'>\" +\n" +
-                "            \"<canvas class='mini-canvas' data-src='/\" + previewFilename + \"'></canvas>\" + \n" +
+                "            \"<img class='thumb' src='/thumbnails/\" + filename + \"?t=\" + Date.now() + \"' onerror='this.onerror=null; setTimeout(()=>this.src=this.src, 2000)'>\" +\n" +                "            \"<canvas class='mini-canvas' data-src='/\" + previewFilename + \"'></canvas>\" + \n" +
                 "            \"</div>\" +\n" +
                 "            \"<div class='info'>\" +\n" +
                 "            \"<div style='font-size:15px; font-weight:bold; color:#ffffff; margin-bottom:4px;'>\" + dateStr + \" &nbsp; \" + timeStr + \"</div>\" +\n" +
